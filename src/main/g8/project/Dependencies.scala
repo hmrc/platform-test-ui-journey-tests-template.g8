7617b1ba828dd1e2bd7b7cf88db23451d70e69d6
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2024-02-23 11:52:40 +0000

    PLATUI-BAU: Update scalatest version to 3.2.18

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
     "com.vladsch.flexmark" % "flexmark-all"   % "0.62.2" % Test,
-    "org.scalatest"       %% "scalatest"      % "3.2.17" % Test,
+    "org.scalatest"       %% "scalatest"      % "3.2.18" % Test,
     "org.slf4j"            % "slf4j-simple"   % "1.7.36" % Test,
     "uk.gov.hmrc"         %% "ui-test-runner" % "0.15.0" % Test
   )
