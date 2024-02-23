d59f5bc75975b21e26937f544a04d27b27edaca9
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2024-02-23 11:53:51 +0000

    PLATUI-BAU: Update ui-test-runner version to 0.19.0

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     "com.vladsch.flexmark" % "flexmark-all"   % "0.64.8" % Test,
     "org.scalatest"       %% "scalatest"      % "3.2.18" % Test,
     "org.slf4j"            % "slf4j-simple"   % "2.0.9"  % Test,
-    "uk.gov.hmrc"         %% "ui-test-runner" % "0.15.0" % Test
+    "uk.gov.hmrc"         %% "ui-test-runner" % "0.19.0" % Test
   )
 
 }
