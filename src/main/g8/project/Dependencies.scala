3c33d3d0c91690c1ef52a7c8a173054eb51e660d
Viv Richards <1014468+vivrichards600@users.noreply.github.com>
2024-03-22 08:40:02 +0000

    PLATUI-BAU - Bump ui-test-runner to 0.23.0

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     "ch.qos.logback"       % "logback-classic" % "1.5.1"  % Test,
     "com.vladsch.flexmark" % "flexmark-all"    % "0.64.8" % Test,
     "org.scalatest"       %% "scalatest"       % "3.2.18" % Test,
-    "uk.gov.hmrc"         %% "ui-test-runner"  % "0.22.0" % Test
+    "uk.gov.hmrc"         %% "ui-test-runner"  % "0.23.0" % Test
   )
 
 }
