a4d6e605777aa361d8a1266f7a9e438490a67857
Tom McArdle <79710668+tmc-ee@users.noreply.github.com>
2024-04-11 17:16:33 +0100

    PLATUI-2962: Update ui-test-runner version to 0.26.0

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     "ch.qos.logback"       % "logback-classic" % "1.5.4"  % Test,
     "com.vladsch.flexmark" % "flexmark-all"    % "0.64.8" % Test,
     "org.scalatest"       %% "scalatest"       % "3.2.18" % Test,
-    "uk.gov.hmrc"         %% "ui-test-runner"  % "0.24.0" % Test
+    "uk.gov.hmrc"         %% "ui-test-runner"  % "0.26.0" % Test
   )
 
 }
